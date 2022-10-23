7622b85ace9c37811b6c6f7f644a9fa4290a48ba
mf.liang <mf.liang@outlook.com>
2022-10-23 19:16:28 +0800

    定义了测试的总体框架

--- env_args.py
+++ env_args.py
@@ -4,4 +4,20 @@
 # @File    : env_args.py
 # @Software: PyCharm
 # @desc    :
+import random
+import subprocess
+import uuid
+from scapy.volatile import RandMAC
 
+# 本机mac
+address = ':'.join(hex(uuid.getnode())[2:][i:i + 2] for i in range(0, len(hex(uuid.getnode())[2:]), 2))
+
+# 事务id(可选)
+xid = random.randint(1, 900000000)
+
+# 客户端mac地址  (重用曾经分配的IP地址，更改这个选项)（更新租约,更改这个选项为当前mac）
+random_mac = RandMAC()
+
+# 获取本机ipv6
+ipv6_src = subprocess.Popen("ip -6 address show | grep inet6 | awk '{print $2}' | cut -d'/' -f1",
+                            shell=True, stdout=subprocess.PIPE)
--- tools.py
+++ tools.py
@@ -4,6 +4,10 @@
 # @File    : tools.py
 # @Software: PyCharm
 # @desc    :
+import logging
+import socket
+import subprocess
+
 
 class Tools:
 
@@ -19,3 +23,20 @@ class Tools:
         else:  # 字符串转化成字节码
             data = bytes.fromhex(data)
         return data
+
+    @staticmethod
+    def get_local_ipv4():
+        # 获取本机IP
+        local_ipv4 = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        local_ipv4.connect(('8.8.8.8', 80))
+        local_ipv4 = local_ipv4.getsockname()[0]
+        logging.debug(f"获取本机IP:\t{local_ipv4}")
+        return local_ipv4
+
+    @staticmethod
+    def get_local_ipv6():
+        # 获取本机ipv6
+        local_ipv6 = subprocess.Popen("ip -6 address show | grep inet6 | awk '{print $2}' | cut -d'/' -f1",
+                                      shell=True, stdout=subprocess.PIPE)
+        local_ipv6 = str(local_ipv6.stdout.readlines()[1], encoding='utf-8').strip('\n')
+        return local_ipv6
